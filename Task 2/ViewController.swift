81e0390b5fa83e3b281adea15cd765df9701eaaf
Julia <yulialapenak@yahoo.com>
2022-03-16 08:35:47 +0300

    Initial Commit

---
+++ Task 2/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Task 2
+//
+//  Created by Юлия Лапенок on 3/16/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
